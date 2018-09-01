83fc4a1fafc0d38e052f4f0659fe59a23a02600c
root <root@laoyouyu3.mylabserver.com>
2018-09-01 12:52:38 +0000

    Third edition - change service_ensure attribute

--- manifests/init.pp
+++ manifests/init.pp
@@ -48,7 +48,7 @@ class ntp(
   String $config_name         = $ntp::params::config_name, 
   String $config_file_mode    = $ntp::params::config_file_mode, 
   Array[String] $ntpservers   = $ntp::params::ntpservers, 
-  String $service_ensure      = $ntp::params::service_ensure, 
+  Boolean $service_ensure      = $ntp::params::service_ensure, 
   String $service_name        = $ntp::params::service_name, 
   Boolean $service_enable     = $ntp::params::service_enable,
   Boolean $service_hasrestart = $ntp::params::service_hasrestart,
--- manifests/params.pp
+++ manifests/params.pp
@@ -4,7 +4,7 @@ class ntp::params {
   $config_name =   'ntp.conf'
   $config_file_mode = '0664' 
   $ntpservers = ['0.centos.pool.ntp.org', '1.centos.pool.ntp.org']
-  $service_ensure = 'running'
+  $service_ensure = true
   $service_enable = true
   $service_hasrestart = true
   $service_hasstatus  = true
